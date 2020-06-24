b8de51dec4a74831d2facae7997cdcfad55bda10
letuan0803 <anhtuan98a2@gmail.com>
2020-06-24 16:57:59 +0700

    tuan 24/6

--- Test2/Controllers/HomeController.cs
+++ Test2/Controllers/HomeController.cs
@@ -33,5 +33,11 @@ namespace Test2.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult Test()
+        {
+            return View();
+        }
+
     }
 }
